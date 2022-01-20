1630864ac8deb8946363adc4487f9c5cf684b265
Rafael Menezes <menezes.inbox@gmail.com>
2022-01-20 17:25:32 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://json-server-rafael.herokuapp.com/',
+  //baseURL: 'http://localhost:3333',
+  baseURL: ' http://localhost:3333/',
 });
